a322b117eeb7416df31d76d16c2096afd97c0ba6
myzhoulang <604389771@qq.com>
2017-08-26 11:08:36 +0800

    设置环境

--- config/address.js
+++ config/address.js
@@ -1,4 +1,14 @@
-module.exports =  {
-  IMG_ADDRESS: 'http://img.jumore.com',
-  SERVER_ADDRESS:'http://webapi.jtt.com'//'http://www.jmexpert.com'
+const envs = {
+  dev:{
+    IMG_ADDRESS: 'http://image.jm.com',
+    SERVER_ADDRESS:'http://webapi.jtt.com'//'http://www.jmexpert.com'
+  },
+
+  production:{
+    IMG_ADDRESS: 'http://img.jumore.com',
+    SERVER_ADDRESS:'http://www.jmexpert.com'
+  }
 }
+// let env = 'dev'
+let env = 'production'
+module.exports =  envs[env];
