66cd576a8f737b4329f8412c4746a75b5fe2b27e
lf171314 <596587761@qq.com>
2017-08-31 09:26:27 +0800

    chore: 添加 editorconfig 文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,12 +80,12 @@ module.exports = {
   */
   extend (config, ctx) {
     if (ctx.isClient) {
-      // config.module.rules.push({
-      //   enforce: 'pre',
-      //   test: /\.(js|vue)$/,
-      //   loader: 'eslint-loader',
-      //   exclude: /(node_modules)/
-      // })
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/
+      })
     }
   }
 }
