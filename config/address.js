37a02ff93077a1d79dfe5fb9f2ccfb4b150fca9d
lf171314 <596587761@qq.com>
2017-08-31 10:33:55 +0800

    chore: 修改 address 文件

--- config/address.js
+++ config/address.js
@@ -1,27 +1,29 @@
 const envs = {
   dev: {
     IMG_ADDRESS: 'http://image.jm.com',
-    SERVER_ADDRESS: 'http://192.168.102.191:8099'
-  },
-
-  test: {
-    IMG_ADDRESS: 'http://image.jm.com',
     SERVER_ADDRESS: 'http://webapi.jtt.com',
     USERCENTER_ADDRESS: 'http://test-uc3.dev.com',
     CENTER_ADDRESS: 'http://center.jtt.com',
-    CDN_ADDRESS: 'http://192.168.102.114:3002/_nuxt/'
-
+    CDN_ADDRESS: 'http://127.0.0.1:3000/_nuxt/'
   },
 
   production: {
+    // 图片服务器
     IMG_ADDRESS: 'http://img.jumore.com',
+    // API 服务器
     SERVER_ADDRESS: 'http://www.jmexpert.com',
+    // 认证中心
     USERCENTER_ADDRESS: 'http://passport.jumore.com/cas/',
+    // 中台地址
     CENTER_ADDRESS: 'http://center.jmexpert.com',
-    // CDN_ADDRESS: 'http://192.168.102.114:3000/_nuxt/'
-    CDN_ADDRESS: 'http://static.jmexpert.com/static/'
-  }
+    // CDN 服务器
+    CDN_ADDRESS: 'http://static.jmexpert.com/static/',
+    // 图片上传服务器
+    UPLOAD_ADDRESS: 'http://image5.jm.com'
+}
 }
 
-let env = 'production'
+let env = process.env.NODE_ENV !== 'production'
+  ? 'dev' : 'production'
+
 module.exports = envs[env]
